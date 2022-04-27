c9b2a8ce3bc5995fc61f4d0834a864f8070825bb
Shivang Soni <shivangsoni22@gmail.com>
2022-04-26 21:32:25 -0700

    Dining service boilerplate code

---
+++ OtripleS.Web.Api/Models/Dining/Dining.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OtripleS.Web.Api.Models.Dining
+{
+    public class Dining
+    {
+    }
+}
